380abf554c018924a09fc13dafaa58295316d6d1
oull3 <oull3@kakao.com>
2024-12-11 10:08:34 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,4 +1,5 @@
-def solution(my_string, letter): #solution 함수 정의
+# solution 함수 정의
+def solution(my_string, letter):
     
-    # replace 함수를 이용하여 값을 return 
+# replace 함수를 이용하여 값을 return 
     return my_string.replace(letter, "")
