8caa6cf6f447d722946073a693dc34126e735a53
ouxrlo <ouxrlo3@gmail.com>
2024-12-17 09:44:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,23 +1,10 @@
 def solution(array):
-    answer = 0
-    from collections import Counter
-
-    # 저는 counter를 딕셔너리로 저장했구만요
-    counter = Counter(array)
-
-    # 숫자가 나온것중에 최대값
-    max_count = max(counter.values())
-
-    
-    if list(counter.values()).count(max_count) > 1:
-        return -1  # 최빈값이 여러 개면 -1 반환하구만요
-    else:
-        # 먹이를 찾으러 다니는 하이에나 처럼 최빈값을 찾아다녀보자구
-        for key, value in counter.items():
-            if value == max_count:
-                answer = key
-                break
-
-    return answer
-
-    return answer
+    num = 0 # 많이 나타난 아이 두둥장 횟수
+    data = 0 # 최빈값
+    for i in set(array):
+        if array.count(i) > num:
+            num = array.count(i)
+            data = i
+        elif array.count(i) == num:
+            data = -1
+    return data
