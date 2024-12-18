a632bc5fb6ff13fe87c8d0bbba478fa7ea39e0b4
ouxrlo <ouxrlo3@gmail.com>
2024-12-18 09:22:23 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,5 +1,13 @@
+# 첫번째 방법
+
 def solution(slice, n):
-    total_slices = n
-    answer = (total_slices + slice - 1) // slice
     
-    return answer
+    pizzas = (n + slice - 1) // slice
+    return pizzas
+
+
+# 두번째 방법 ()
+
+#def solution(slice, n):
+    #answer = (n - 1) // slice + 1
+
