887b3f05ced296f338653b4f5020811b8079501f
ouxrlo <ouxrlo3@gmail.com>
2024-12-20 08:37:57 +0900

    [level 0] Title: 그림 확대, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,4 +1,14 @@
 def solution(picture, k):
+    result = []
+    for p in picture:
+        s = ''
+        for a in p:
+            s += a * k
+        for i in range(k):
+            result.append(s)
+    return result
+'''
+def solution(picture, k):
     answer = []
     
 # 저는 먼저 가로로 k배 늘려주겠다요
@@ -10,3 +20,4 @@ def solution(picture, k):
             answer.append(up) # 세로로 늘려주면 가로x세로 k배 늘리기 성공적
         
     return answer
+    '''
