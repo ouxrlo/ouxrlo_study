6c51a1e911fbbc46b1be2b7dc60d610c9cf9d5fd
ouxrlo <ouxrlo3@gmail.com>
2024-12-19 19:39:09 +0900

    [level 0] Title: 그림 확대, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,12 +1,12 @@
 def solution(picture, k):
     answer = []
     
-# 가로로 먼저 k배 늘려주기
+# 저는 먼저 가로로 k배 늘려주겠다요
 
     for row in picture:
-        increase_row =''.join([ch*k for ch in row]) #join과 리스트 컴프리헨션 사용
+        up =''.join([ch*k for ch in row]) #join과 리스트 컴프리헨션 사용
     
-        for x in range(k): # 이미 가로로 k배 늘려진 상태
-            answer.append(increase_row) # 세로로 늘려주면 가로x세로 k배 늘리기 성공적
+        for x in range(k): # 이미 가로로 k배 불려진 상태다요
+            answer.append(up) # 세로로 늘려주면 가로x세로 k배 늘리기 성공적
         
     return answer
