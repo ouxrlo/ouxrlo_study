a65372d2073fb65f7dae43e3b2cec7258fbb3877
ouxrlo <ouxrlo3@gmail.com>
2024-12-19 17:17:31 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,10 +1,13 @@
 def solution(my_string, is_prefix):
     answer = 0
-    # 접두사 - 문자열의 처음 부분
+    
     ip=len(is_prefix)
-    # my_string의 처음부터 is_prefix 길이만큼 자른 부분이 is_prefix와 같은지 확인합니다.
+    
+    # my_string의 처음부터 is_prefix 길이만큼 자른 부분 = is_prefix 확인
+    
     if my_string[:ip] == is_prefix:
-        return 1
+        return 1 # 접두사면 1
     else:
-        return 0
+        return 0 # 아니면 0
+    
     return answer
