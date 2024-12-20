ac53377410b270570a2b6fe695ae02863552897f
ouxrlo <ouxrlo3@gmail.com>
2024-12-20 15:47:59 +0900

    [level 0] Title: 그림 확대, Time: 0.61 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,8 +1,11 @@
 def solution(picture, k):
-    answer = []
+    answer = [] # 1.초기화
 
-    for line in picture:
-        for _ in range(k):
-            answer.append(''.join([x * k for x in line]))
+    for line in picture:  # 각 열을 처리
+        for _ in range(k):  # k배 증가
+            answer.append(''.join([f * k for f in line]))  # 4.세로로 k배 증가, 7.세로로 k배 증가=> 증가시킨 다음에 빈 문자열에 넣음
+                                    # 리스트 -> 3.line = "AB", 가로로 k배 증가 => 빈 문자열에 넣음
+                                    # 컴프리헨션 -> 6.line = "CD", 가로로 k배 증가 => 빈 문자열에 넣음
+                            
+    return answer   # 8. answer 을 리턴
 
-    return answer
