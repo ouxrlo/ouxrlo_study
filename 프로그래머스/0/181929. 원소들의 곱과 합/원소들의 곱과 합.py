a2e5a0100fa0b881597b737855638fd584f41f6b
ouxrlo <ouxrlo3@gmail.com>
2024-12-26 18:55:46 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -7,3 +7,12 @@ def solution(num_list):
     p = sum(num_list)**2
     
     return 1 if x<p else 0 
+
+
+# def solution(num_list):
+#     sum = 0
+#     mul = 1
+#     for n in num_list:
+#         sum += n
+#         mul *= n
+#     return int(mul < sum**2)
