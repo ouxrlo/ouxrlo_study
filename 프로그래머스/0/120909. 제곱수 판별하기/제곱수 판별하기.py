cc3be099e85746bea1a06f7fbf890ca514465298
ouxrlo <ouxrlo3@gmail.com>
2024-12-27 09:55:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 9.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -9,14 +9,20 @@
 # 머리 비우고 보이는데로 적어봤는데 역시 답이 아니래 ㅎ,,
 # 그래서 지피티한테 제곱근 구하는 식이 따로 있냐고 물어봄,,,
 
-import math
+# import math
+
+# def solution(n):
+    
+#     sqrt_n = math.sqrt(n) # 얘가 n의 제곱근 식 
+    
+#     if sqrt_n.is_integer(): # is_integer() 계산 된 제곱근이 정수 인지
+#         return 1
+#     else:
+#         return 2
 
 def solution(n):
-    
-    sqrt_n = math.sqrt(n) # 얘가 n의 제곱근 식 
-    
-    if sqrt_n.is_integer(): # is_integer() 계산 된 제곱근이 정수 인지
-        return 1
-    else:
-        return 2
+    for i in range(1, n // 2):
+        if i * i == n:
+            return 1
+    return 2
     
