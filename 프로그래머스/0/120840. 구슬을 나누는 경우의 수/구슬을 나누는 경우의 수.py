38b78b01458315a62c5e0cf74aaa3216e2d14f5f
ouxrlo <ouxrlo3@gmail.com>
2024-12-31 09:29:07 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,9 +1,22 @@
+# 1
+def solution(balls, share):
+    def factorial(n):
+        result = 1  
+        for i in range(1, n + 1): 
+            result *= i
+        return result
 
+    def combination(b, s):
+        return factorial(b) // (factorial(s) * factorial(b - s))
+
+    return combination(balls, share)
+
+# 위와 같이 풀었는데 너무 길어 줄일수 없나 생각해보다가 아래와 같이 정리해봤더니 훨씬 깔꼼
 
 from math import factorial as f
 
 def solution(balls, share):
-    return f(balls) // f(share) // f(balls - share)
+    return f(b) // f(s) // f(b - s)
 
 # 2
 
@@ -13,4 +26,3 @@ import math
 def solution(balls, share):
     return math.comb(balls, share)
 
-# 3
