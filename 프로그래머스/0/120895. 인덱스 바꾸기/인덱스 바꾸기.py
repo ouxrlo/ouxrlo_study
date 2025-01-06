f3baf305260bd1e184fbe888592489ed0ba9795e
ouxrlo <ouxrlo3@gmail.com>
2025-01-06 09:40:19 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,11 +1,11 @@
 def solution(my_string, num1, num2):
     # 문자열 수정 불가능 -> 리스트로 바꿔줘야함.
-    char_list = list(my_string)
+    c = list(my_string)
     
     # 문자열을 바꿔줘야함
-    char_list[num1], char_list[num2] = char_list[num2], char_list[num1]
+    c[num1], c[num2] = c[num2], c[num1]
     
     # join() 사용 후 , 리스트를 다시 문자열로 바꿔줌
-    result = ''.join(char_list)
+    result = ''.join(c)
     
     return result
