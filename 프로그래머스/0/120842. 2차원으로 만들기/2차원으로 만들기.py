f013bd71c1268c566109db1635bfbfc4d62806dc
ouxrlo <ouxrlo3@gmail.com>
2025-01-06 19:12:20 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,7 +1,15 @@
-def solution(num_list, n):
-    answer = []
+# def solution(num_list, n):
+#     answer = []
     
-    for i in range(0, len(num_list), n):  
-        answer.append(num_list[i:i + n])  
+#     for i in range(0, len(num_list), n):  
+#         answer.append(num_list[i:i + n])  
  
-    return answer
+#     return answer
+
+def solution(num_list, n):
+    if not num_list:
+        return []
+        
+    row = num_list[:n]
+    part = solution(num_list[n:], n)
+    return [row]+part
