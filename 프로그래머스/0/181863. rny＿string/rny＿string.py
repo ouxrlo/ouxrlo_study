4bfca35944ee96aa0a274c86dc289dbbef441ce3
ouxrlo <ouxrlo3@gmail.com>
2025-01-09 08:44:32 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.py
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -1,8 +1,2 @@
 def solution(rny_string):
-    answer = ''  # 결과를 저장할 빈 문자열 준비
-    for i in rny_string:  # 문자열의 각 글자를 순서대로 확인
-        if i == 'm':  # 현재 글자가 'm'이면
-            answer += 'rn'  # 'rn'을 추가
-        else:  # 현재 글자가 'm'이 아니면
-            answer += i  # 해당 글자를 그대로 추가
-    return answer  # 최종 결과 반환
+    return ''.join(['rn' if i == 'm' else i for i in rny_string])  
