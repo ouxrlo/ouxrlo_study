316e3d4b0d74616b40a271bfb773a9678e932c21
ouxrlo <ouxrlo3@gmail.com>
2025-01-09 12:58:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.64 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -8,4 +8,16 @@ def solution(n):
         if count >= 3:
             answer += 1
         
+        
+    return answer
+
+
+
+def solution(n):
+    answer = 0
+    for i in range(4, n + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                answer += 1
+                break
     return answer
