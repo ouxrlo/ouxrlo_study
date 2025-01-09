ddaecfd1c92818f8d398ad2273600abbc83e8e6e
ouxrlo <ouxrlo3@gmail.com>
2025-01-09 14:15:55 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.07 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,12 +1,30 @@
+# 1
 def solution(n):
     answer = []
     for i in range(n):
         row = []
         for j in range(n):
-            if i == j:
+            if i ==j:
                 row.append(1)
             else:
                 row.append(0)
         answer.append(row)
+            
     return answer
     
+    
+# 2
+def solution(n):
+    return [[int(i==j)for i in range(n)]for j in range(n)]
+
+
+
+# 3
+def solution(n):
+    answer = []
+    for i in range(n):
+        arr = [0] * n
+        arr[i] = 1
+        answer.append(arr)
+        
+    return answer
