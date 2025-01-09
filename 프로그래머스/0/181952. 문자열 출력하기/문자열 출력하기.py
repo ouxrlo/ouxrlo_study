4a4358fcc1213f9a6e6bf982f54a64ffeaa2b5c9
ouxrlo <ouxrlo3@gmail.com>
2025-01-09 16:02:10 +0900

    [level 0] Title: 문자열 출력하기, Time: 12.69 ms, Memory: 7.27 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.py
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.py
@@ -1,14 +1,4 @@
-# 문자열 입력 받기
-# 1 ≤ str의 길이 ≤ 1,000,000
-# str에는 공백이 없으며, 한 줄로만 주어짐
-import sys
-
-# 입력을 빠르게 처리하기 위해 sys.stdin.read() 사용
-str = sys.stdin.read().strip()
-
-# 문자열 출력
-print(str)
-
+print(input())
 
 
 
