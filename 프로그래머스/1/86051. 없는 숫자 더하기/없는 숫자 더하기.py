54fed7c5d290fe52a2cd45626125bcb9466b24b0
ouxrlo <ouxrlo3@gmail.com>
2025-01-09 16:12:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,12 +1,7 @@
-# def solution(numbers):
-#     return 45-sum(numbers)
-# # 0~9 까지 합 - 찾을 수 있는 배열 합 = 찾을 수 없는 배열 합
-
 def solution(numbers):
-   
-    a = sum(range(10))  
-    b = sum(numbers) 
-    return a - b
+    return 45-sum(numbers)
+# 0~9 까지 합 - 찾을 수 있는 배열 합 = 찾을 수 없는 배열 합
+
 
 
 
