532f655c8db26d0f005e6fd1b1c254a571dc4d19
ouxrlo <ouxrlo3@gmail.com>
2025-01-17 09:00:39 +0900

    [level 0] Title: qr code, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -1,6 +1,6 @@
 def solution(q, r, code):
     answer = ''
-    for index in range(len(code)):
-        if index % q == r:
-            answer += code[index]
+    for i in range(len(code)):
+        if i % q == r:
+            answer += code[i]
     return answer
