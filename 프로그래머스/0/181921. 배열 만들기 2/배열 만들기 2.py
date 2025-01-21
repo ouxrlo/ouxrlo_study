1737802b456198cb54f48f7bb3de41e8ca9931f2
ouxrlo <ouxrlo3@gmail.com>
2025-01-21 09:10:27 +0900

    [level 0] Title: 배열 만들기 2, Time: 487.44 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,6 +1,7 @@
 def solution(l, r):
     answer = []
     for num in range(l, r + 1):
-        if all(i in '05' for i in str(num)):
+        if all(i in '05' for i in str(num)):# "05" 모든 자릿수가 '0' 또는 '5'로만 이루어졌는지 확인
             answer.append(num)
     return answer if answer else [-1]
+
