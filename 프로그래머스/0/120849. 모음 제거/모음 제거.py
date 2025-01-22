575391e82a642374f8efdfdc09c11307089d47aa
ouxrlo <ouxrlo3@gmail.com>
2025-01-23 05:48:09 +0900

    [level 0] Title: 모음 제거, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,2 @@
 def solution(my_string):
-    answer = ''
-    for x in my_string:
-        if x not in "aeiou":  # 모음이 아니면
-            answer += x  # answer에 추가
-    return answer
-        
+    return "".join([x for x in my_string if not(x in "aeiou")])
