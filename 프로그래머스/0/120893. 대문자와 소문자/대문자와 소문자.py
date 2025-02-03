682e70dc8e01b1ef7e6a86f3eeee6ef020f37d55
ouxrlo <ouxrlo3@gmail.com>
2025-02-03 11:28:01 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.isupper():
+            answer += i.lower()
+        elif i.islower():
+            answer += i.upper()
+        
+    return answer
