ac791f23f94ae963ed11da8e606f18e8af3210cd
ouxrlo <ouxrlo3@gmail.com>
2025-02-05 17:21:16 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    answer = 0
+    
+    for num in num_list:
+        count = 0
+        
+        while num > 1:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num = (num - 1) // 2
+            
+            count += 1
+        
+        answer += count
+
+    return answer
