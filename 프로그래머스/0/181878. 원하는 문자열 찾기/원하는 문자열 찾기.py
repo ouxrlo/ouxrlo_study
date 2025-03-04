e896a65359b13ce50375b470180d7a753b3709e2
ouxrlo <ouxrlo3@gmail.com>
2025-03-04 09:07:00 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,8 +1,2 @@
 def solution(myString, pat):
-    myString = myString.lower() # lower() 소문자로 바꿔주는 것
-    pat = pat.lower()
-    
-    if pat in myString:
-        return 1
-    else:
-        return 0
+     return 1 if pat.lower() in myString.lower() else 0
